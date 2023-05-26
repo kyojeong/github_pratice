10b22c5f91e1f8767030492fdbfbd9ee0653f747
kyojeong <37138418+kyojeong@users.noreply.github.com>
2023-05-26 17:55:24 +0800

    Update code.cpp

--- 837. New 21 Game/code.cpp
+++ 837. New 21 Game/code.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if(k==0) return 1;
+        vector<float> dp(k+maxPts,0);
+        for(int i=k;i<=n;i++){
+            dp[i] = 1;
+        }
 
+        for(int i=(k-1);i>=0;i--){
+            for(int j=1;j<=maxPts;j++){
+                dp[i] += dp[i+j];
+            }
+            dp[i] = dp[i] / maxPts;
+        }
+        return dp[0];
+    }
+};
