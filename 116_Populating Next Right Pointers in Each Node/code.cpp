15353504ee033213c00fa7bc7d7075a84a8e5506
kyojeong <37138418+kyojeong@users.noreply.github.com>
2023-08-08 20:32:52 +0800

    Create code.cpp

---
+++ 116_Populating Next Right Pointers in Each Node/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL) return root;
+
+        if(root->right != NULL){
+            root->left->next = root->right;
+            if(root->next != NULL){
+                root->right->next = root->next->left;
+            }
+        }
+
+        connect(root->left);
+        connect(root->right);
+        return root;
+    }
+};
